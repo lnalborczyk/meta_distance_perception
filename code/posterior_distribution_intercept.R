89a7e97488f2a03a8db5099f101214678d1c4170
lnalborczyk <ladislas.nalborczyk@gmail.com>
2019-07-24 17:07:00 +0200

    adding the effect size plot

--- code/posterior_distribution_intercept.R
+++ code/posterior_distribution_intercept.R
@@ -1,9 +1,10 @@
+library(tidyverse)
 library(BEST) # posterior distribution plot
 
 post <- posterior_samples(bmod1)
 
 plotPost(
-    post$b_intercept, showCurve = TRUE, showMode = FALSE,
+    post$b_intercept, showCurve = TRUE, showMode = TRUE,
     credMass = NULL, xlab = "", xlim = c(0, 0.6)
     )
 
@@ -11,12 +12,27 @@ intercept <- post$b_intercept
 mu <- mean(intercept)
 sigma <- sd(intercept)
 
-abline(v = mu + sigma, lty = 2)
-abline(v = mu + 2 * sigma, lty = 2)
-abline(v = mu + 3 * sigma, lty = 2)
-abline(v = mu - sigma, lty = 2)
-abline(v = mu - 2 * sigma, lty = 2)
-abline(v = mu - 3 * sigma, lty = 2)
+# quantiles_intercept <- quantile(
+#     intercept, probs = c(0.05, 0.125, 0.25, 0.75, 0.875, 0.95)
+#     )
+
+hdi_50 <- hdi(intercept, credMass = 0.5) %>% as.numeric
+hdi_80 <- hdi(intercept, credMass = 0.8) %>% as.numeric
+hdi_95 <- hdi(intercept, credMass = 0.95) %>% as.numeric
+
+# abline(v = quantiles_intercept[1], lty = 2)
+# abline(v = quantiles_intercept[2], lty = 2)
+# abline(v = quantiles_intercept[3], lty = 2)
+# abline(v = quantiles_intercept[4], lty = 2)
+# abline(v = quantiles_intercept[5], lty = 2)
+# abline(v = quantiles_intercept[6], lty = 2)
+
+abline(v = hdi_50[1], lty = 2)
+abline(v = hdi_50[2], lty = 2)
+abline(v = hdi_80[1], lty = 2)
+abline(v = hdi_80[2], lty = 2)
+abline(v = hdi_95[1], lty = 2)
+abline(v = hdi_95[2], lty = 2)
 
 library(export) # export to ppt
-graph2ppt(file = "/Users/Ladislas/Desktop/plotpost.pptx", width = 10, height = 10, append = TRUE)
+graph2ppt(file = "plotpost.pptx", width = 10, height = 10, append = FALSE)
